b5c0ddf8629db6b9debb20dcaa5da6368e5cfeed
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-29 18:35:42 +0530

    Simple use of Work manager

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -17,11 +17,32 @@
 
 package com.example.android.devbyteviewer.work
 
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
 // TODO (01) Create the RefreshDataWorker class, extend it from CoroutineWorker, and
-// pass in a Context and WorkerParams.
+//  pass in a Context and WorkerParams.
+class RefreshDataWorker(appContext: Context, params: WorkerParameters):
+        CoroutineWorker(appContext, params) {
 
-// TODO (02) Override the required doWork() method, and create variables for the
-// database and the repository.
+    // TODO (02) Override the required doWork() method, and create variables for the
+    //  database and the repository.
+    override suspend fun doWork(): Payload {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
 
-// TODO (03) Inside doWork(), in a try-catch block, refresh the videos, and
-// use Payload() to return SUCCESS or RETRY result.
+        // TODO (03) Inside doWork(), in a try-catch block, refresh the videos, and
+        //  use Payload() to return SUCCESS or RETRY result.
+        return try {
+            repository.refreshVideos()
+            Payload(Result.SUCCESS)
+        } catch (e: HttpException) {
+            Payload(Result.RETRY)
+        }
+    }
+
+}
