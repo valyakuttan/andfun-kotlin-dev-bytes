676523513929807a44c624edd9cf27a64e2db70b
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-29 10:14:41 +0530

    DAO created to access data from Room

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,8 +17,21 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
 // TODO (01) Create a @Dao interface called VideoDao.
+@Dao
+interface VideoDao {
 
-// TODO (02) Add SQL @Query getVideos() function that returns a List of DatabaseVideo.
+    // TODO (02) Add SQL @Query getVideos() function that returns a List of DatabaseVideo.
+    @Query("select * from databasevideo")
+    fun getVideos(): List<DatabaseVideo>
 
-// TODO (03) Add SQL @Insert insertAll() that replaces on conflict (or upsert).
+    // TODO (03) Add SQL @Insert insertAll() that replaces on conflict (or upsert).
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+
+}
